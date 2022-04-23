bfce81e498e2ccf91d06aa61feaa593fdc93da81
mjmax75 <minji.shon.415@gmail.com>
2022-04-23 00:23:22 -0500

    portfolio section completed

--- script.js
+++ script.js
@@ -26,3 +26,58 @@ function linkAction() {
 }
 
 navLink.forEach((link) => link.addEventListener("click", linkAction));
+
+//========== TYPEIT LIBRARY FOR THE SUBTITLE ==========
+const typeItSubtitle = document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    new TypeIt("#home-subtitle", {
+      loop: true,
+      speed: 120,
+    })
+      .pause(600)
+      .delete(18, { delay: 600 })
+      .type("Data Analyst")
+      .pause(600)
+      .go();
+  }
+);
+
+//==================== ACCORDION SKILLS ====================
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+//==================== MIXITUP FILTER PORTFOLIO ====================
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
+// LINK ACTIVE WORK
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach((link) => link.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((list) => list.addEventListener("click", activeWork));
